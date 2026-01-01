7bf79129c3da6dfed8cbf2a22c4f9186ea276dee
Mahamudul Hasan <hasanjasin@gmail.com>
2026-01-01 23:02:40 +0600

    get and update from xl

--- Section_13_ExcelUtils_with_Playwright/ExcelJSUtil/excelDemo.js
+++ Section_13_ExcelUtils_with_Playwright/ExcelJSUtil/excelDemo.js
@@ -1,35 +1,44 @@
 const ExcelJs = require("exceljs")
 
 
-async function excelTest() {
+async function writeExcelTest(searchText, replaceText, change, filePath) {
 
-    let output = {row:-1, column:-1}
+    
     // Create a new workbook
     const workbook = new ExcelJs.Workbook()
-
     // Read an existing Excel file
-    await workbook.xlsx.readFile('download.xlsx')
+    await workbook.xlsx.readFile(filePath)
     // Add a worksheet to the workbook
-    const wokrsheet = workbook.getWorksheet('Sheet1')
+    const workSheet = workbook.getWorksheet('Sheet1')
+    const output = await readExcelTest(workSheet, searchText)
 
 
+    const cell = workSheet.getCell(output.row, output.column+change.colChange)
+    cell.value = replaceText
+    // Save the modified workbook
+    await workbook.xlsx.writeFile(filePath)
+}
+
+
+async function readExcelTest(workSheet, searchText){
+
+    let output = {row:-1, column:-1}
     // Accessing rows and cells
-    wokrsheet.eachRow((row, rowNumber) => {
+    workSheet.eachRow((row, rowNumber) => {
         // Log each cell value in the row
         row.eachCell((cell, colNumber) => {
             //console.log(cell.value)
-            if(cell.value ==='Apple'){
+            if(cell.value === searchText){
                 output.row = rowNumber
                 output.column = colNumber
             }
         })
     })
+    return output
 
-    const cell = wokrsheet.getCell(output.row, output.column)
-    cell.value = "Murgi"
-
-    // Save the modified workbook
-    await workbook.xlsx.writeFile('download.xlsx')
 }
 
-excelTest()
+
+
+
+writeExcelTest("Murgi", "999",{rowChange:0, colChange:2}, "download.xlsx")
