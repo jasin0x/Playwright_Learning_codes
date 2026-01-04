5e2c9503638ede036380fd25453a6ef5fdb31eeb
Mahamudul Hasan <hasanjasin@gmail.com>
2026-01-04 14:14:13 +0600

    implement playwright with file download and upload

---
+++ Section_13_ExcelUtils_with_Playwright/ExcelJSUtil/excelDemo_2.js
@@ -0,0 +1,30 @@
+const ExcelJs = require('exceljs');
+const { test, expect } = require('@playwright/test');
+ 
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+  const workbook = new ExcelJs.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet('Sheet1');
+  const output = readExcel(worksheet, searchText); // not async
+ 
+  const cell = worksheet.getCell(output.row, output.column + change.colChange);
+  cell.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
+}
+ 
+// This does no async work, so don't mark it async.
+function readExcel(worksheet, searchText) {
+  let output = { row: -1, column: -1 };
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      if (cell.value === searchText) {
+        output = { row: rowNumber, column: colNumber };
+      }
+    });
+  });
+  return output;
+}
+ 
+//update Mango Price to 350. 
+writeExcelTest("Mango",350,{rowChange:0,colChange:2},"/Users/mahamudulhasan/downloads/download.xlsx");
+
